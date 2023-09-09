cd43b46603fb36b84efba8c2be698aee3179b99d
Fathurahmanalf <113759542+Fathurahmanalf@users.noreply.github.com>
2023-09-09 13:54:29 +0700

    Tugas Homework Minggu 4

---
+++ Tugas week 4/tugashw4.js
@@ -0,0 +1,111 @@
+// Array bilangan 100
+const nialiarray = [];
+for (let i = 0; i < 50 * 2; i++) {
+  let i = Math.random() * 2;
+  nialiarray.push(i);
+  console.log(i);
+}
+
+// Array bilangan genap 
+const arrayGenap = [];
+for (let i = 2; i <= 100; i += 2) {
+  arrayGenap.push(i);
+}
+
+// Array bilangan ganjil 
+const arrayGanjil = [];
+for (let i = 1; i <= 99; i += 2) {
+  arrayGanjil.push(i);
+}
+
+// Menghitung nilai minimum
+function hitungMin(arr) {
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+// Menghitung nilai maksimum 
+function hitungMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+// Menghitung total 
+function hitungTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+// Menghitung rata-rata
+function hitungRataRata(arr) {
+  const total = hitungTotal(arr);
+  return total / arr.length;
+}
+
+// Fungsi untuk menghitung statistik (min, max, total, dan rata-rata) dari sebuah array
+function hitungperbandingan(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    total += arr[i];
+  }
+
+  const rataRata = total / arr.length;
+  return { min, max, total, rataRata };
+}
+
+
+// Menghitung min,max,total,dan rata-rata dari array genap dan ganjil
+const minGenap = hitungMin(arrayGenap);
+const maxGenap = hitungMax(arrayGenap);
+const totalGenap = hitungTotal(arrayGenap);
+const rataRataGenap = hitungRataRata(arrayGenap);
+
+const minGanjil = hitungMin(arrayGanjil);
+const maxGanjil = hitungMax(arrayGanjil);
+const totalGanjil = hitungTotal(arrayGanjil);
+const rataRataGanjil = hitungRataRata(arrayGanjil);
+
+console.log("\nHasil Array Bilangan Genap:");
+console.log("Min:", minGenap);
+console.log("Max:", maxGenap);
+console.log("Total:", totalGenap);
+console.log("Rata-rata:", rataRataGenap);
+
+console.log("\nHasil Array Bilangan Ganjil:");
+console.log("Min:", minGanjil);
+console.log("Max:", maxGanjil);
+console.log("Total:", totalGanjil);
+console.log("Rata-rata:", rataRataGanjil);
+
+// Menghitung perbandingan untuk array genap dan ganjil
+const perbandinganGenap = hitungperbandingan(arrayGenap);
+const perbandinganGanjil = hitungperbandingan(arrayGanjil);
+
+// Menampilkan hasil perbandingan 
+console.log("\nPerbandingan Array Bilangan Genap:");
+console.log(perbandinganGenap);
+
+console.log("\nPerbandingan Array Bilangan Ganjil:");
+console.log(perbandinganGanjil);
